56ce841e104a62e3e345f2e7f037947ffda89a57
Robin <robinhossen1545@gmail.com>
2022-11-08 15:32:38 +0600

    Done

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const generateBtn = document.querySelector('.generate-btn');
 
 generateBtn.addEventListener('click', generateRandomNumber);
 
+values = [];
 
 function generateRandomNumber () {
+    leftTry.innerHTML = 3;
+    submitBtn.disabled = false;
     formControl1.value = Math.floor(Math.random() * (9999 - 1000 + 1) ) + 1000;
+    formControl2.value = '';
+    formControl2.style.color = 'white';
+    values = [];
+    document.getElementById('disabledTry').style.display = 'block';
 }
 
 
@@ -27,7 +34,7 @@ for(let i=0; i<buttons.length; i++){
     buttons[i].addEventListener('click', handleClick);
 }
 
-let values = [];
+// let values = [];
 
 function handleClick (e) {
     if(values.length <= 3 && values.length >= 0){
@@ -44,52 +51,45 @@ backWard.addEventListener('click', function () {
     const copyValues = values;
     copyValues.pop();
     formControl2.value = copyValues.join('');
+    formControl2.style.color = 'white';
 })
 
 clear.addEventListener('click', function () {
     values = [];
     formControl2.value = '';
+    formControl2.style.color = 'white';
 })
 
 const leftTry = document.getElementById('leftTry');
 
  submitBtn.addEventListener('click', function () {
-    //Right Wrong Part Section
+    //Right Wrong Section
     const right = document.getElementById('right');
     const wrong = document.getElementById('wrong');
     if(formControl1.value && formControl2.value) {
         if(formControl1.value == formControl2.value) {
             right.style.display = 'block';
             wrong.style.display = 'none';
-            setInterval(() => {
-                right.style.display = 'none';
-                wrong.style.display = 'none';
-            }, 1000);
+            formControl2.style.color = 'white';
+            values = [];
+            document.getElementById('disabledTry').style.display = 'none';
+            setTimeout(() => {
+                formControl1.value = '';
+                formControl2.value = '';
+            },500);
         }else {
             wrong.style.display = 'block';
             right.style.display = 'none';
-            setInterval(() => {
-                right.style.display = 'none';
-                wrong.style.display = 'none';
-            }, 1000);
-        };
-        // Remaining Effort Section
-    let remainingChance = leftTry.innerHTML;
-    remainingChance--;
-    leftTry.innerHTML = remainingChance;
-    if(remainingChance == 0) {
-        submitBtn.disabled = true;
-        generateBtn.disabled = true;
-        formControl1.disabled = true;
-        formControl2.disabled = true;
+            formControl2.style.color = 'red';
+            // Remaining Effort Section
+        let remainingChance = leftTry.innerHTML;
+        remainingChance--;
+        leftTry.innerHTML = remainingChance;
+        if(remainingChance == 0) {
+            submitBtn.disabled = true;
+            formControl1.disabled = true;
+            formControl2.disabled = true;
+        }
+        };  
     }
-    }
-    
-    
-    // All Clear Section
-    setTimeout(() => {
-        values = [];
-        formControl2.value = '';
-        formControl1.value = ''
-    },600)
 })
